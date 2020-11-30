78482218d65078ab675aecfc61383422af2b8518
gagecglenn@gmail.com <gagecglenn@gmail.com>
2020-11-30 15:12:32 -0700

    Add project files.

---
+++ NueralNetworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NueralNetworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
